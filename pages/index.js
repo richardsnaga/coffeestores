4111e936719b0b9b54b6b1dc9f273ea5735a0331
Richard Sinaga <richardsinaga261@gmail.com>
2022-06-21 21:13:29 +0700

    context, reducer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState, useContext } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -6,6 +6,7 @@ import Banner from "../component/banner";
 import Card from "../component/card";
 import { fetchCoffeeStores } from "../lib/coffee-stores";
 import useTrackLocation from "../hooks/use-track-location";
+import { ACTION_TYPES, StoreContext } from "../pages/_app";
 
 export async function getStaticProps(context) {
   const coffeeStores = await fetchCoffeeStores();
@@ -19,27 +20,40 @@ export async function getStaticProps(context) {
 export default function Home(props) {
   console.log("props = ", props);
 
-  const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
+  const { handleTrackLocation, locationErrorMsg, isFindingLocation } =
     useTrackLocation();
 
-  console.log({ latLong, locationErrorMsg });
+  // const [coffeeStores, setCoffeeStores] = useState("");
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null);
+
+  const { dispatch, state } = useContext(StoreContext);
+
+  const { coffeeStores, latLong } = state;
+
+  console.log("INI LATLONG", { latLong, locationErrorMsg });
 
   useEffect(() => {
     async function setCoffeeStoresByLocation() {
       if (latLong) {
         try {
-          const fetchCoffeeStores = await fetchCoffeeStores(latLong, 30);
-          console.log({ fetchCoffeeStores });
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
+          console.log("FETCHED = ", { fetchedCoffeeStores });
+          // setCoffeeStores(fetchedCoffeeStores);
+          dispatch({
+            type: ACTION_TYPES.SET_COFFEE_STORES,
+            payload: { coffeeStores: fetchedCoffeeStores },
+          });
           // set coffe stores
         } catch (error) {
           //set error
           console.log(error);
+          setCoffeeStoresError(error.message);
         }
       }
     }
 
     setCoffeeStoresByLocation();
-  }, [latLong]);
+  }, [dispatch, latLong]);
 
   const handleOnBannerBtnClick = () => {
     console.log("Hi banner button");
@@ -59,9 +73,32 @@ export default function Home(props) {
           handleOnClick={handleOnBannerBtnClick}
         />
         {locationErrorMsg && <p>Something went wrong : {locationErrorMsg}</p>}
+        {coffeeStoresError && <p>Something went wrong : {coffeeStoresError}</p>}
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} alt="" />
         </div>
+        {coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores near me</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                    className={styles.card}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
+
         {props.coffeeStores.length > 0 && (
           <div className={styles.sectionWrapper}>
             <h2 className={styles.heading2}>Toronto Stores</h2>
