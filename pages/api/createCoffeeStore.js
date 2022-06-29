d413905206b45d04ea607d8d345d7d37fe75b63a
Richard Sinaga <richardsinaga261@gmail.com>
2022-06-29 14:26:56 +0700

    airtable create coffeestore

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -2,5 +2,56 @@ const Airtable = require("airtable");
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   process.env.AIRTABLE_BASE_KEY
 );
-
 const table = base("coffee-stores");
+console.log({ table });
+const createCoffeeStore = async (req, res) => {
+  console.log({ req });
+  if (req.method === "POST") {
+    //find a record
+
+    const { id, name, neighbourhood, address, imgUrl, voting } = req.body;
+
+    try {
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id=${id}`,
+        })
+        .firstPage();
+
+      console.log({ findCoffeeStoreRecords });
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        //create a record
+        const createRecords = await table.create([
+          {
+            fields: {
+              id,
+              name,
+              address,
+              neighbourhood,
+              voting,
+              imgUrl,
+            },
+          },
+        ]);
+        const records = createRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      }
+    } catch (err) {
+      console.error("Error finding store", err);
+      res.status(500);
+      res.json({ message: "Error finding store", err });
+    }
+  }
+};
+export default createCoffeeStore;
